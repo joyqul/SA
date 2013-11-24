f623ac8c388857cef7ecd6cbc9747fa855ff39b5
Joyqul Chen <joyqul@joyqul.nba.nctu.edu.tw>
2013-11-24 16:46:58 +0800

    add p1.awk and p1.sh

---
+++ p1.sh
@@ -0,0 +1,1 @@
+ls -AlR | grep ^[-d] | sort -nr -k 5 | awk ' { if ($1 !~/d.*/) { { size += $5 } { file++ } } if ($1 ~/d.*/) { dir++ } } { if (NR <= 5) {print NR ": "$5" "$9 }} END { { print "Dir num: " dir } { print "File num: " file } { print "total: " size } }'
