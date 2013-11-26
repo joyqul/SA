6f451163e41b970401c95707be7fa89f4326e32c
Joyqul Chen <joy1357924680@gmail.com>
2013-11-26 16:56:46 +0800

    finish command using by gnuplot

--- gnuplot.cmd
+++ gnuplot.cmd
@@ -1,6 +1,6 @@
 set terminal png
 set output 'test.png'
-set style data filledcurve
+set style data filledcurve above y1=0
 set grid
 set title 'CPU Usage'
 set xlabel 'time from now (sec)'
@@ -9,7 +9,6 @@ set ytics 20
 set sample 600
 set xrange[-600:0]
 set yrange[0:100]
-f(x) = x
-plot 'cpuusage' with linespoints, f(x)
+plot "cpuusage_tmp" using 1:2 lt rgb "#87CEFA" notitle
 set output
 quit
