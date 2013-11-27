fdf112f8fad87d826bd19fa0ccb054e21b5d6c58
Joyqul Chen <joy1357924680@gmail.com>
2013-11-27 11:39:19 +0800

    finish monitor's ps format, sort by cpuusage and calculate cpuusage

--- sysmonitor.sh
+++ sysmonitor.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 if [ -z $1 ]; then
     echo "Usage: sysmonitor --logfile filename --email yourname@server"
-    $0 -a &
+#    $0 -a &
     exit
 else
     # Check id
     if [ "$(id -u)" != "0" ]; then
         echo "This script must be run as root" 1>&2
-        $0 -a &
         exit 1
     fi
     while [ 1 ] 
     do
-        ps aux > ps_tmp
+        ps ax -o pid -o user -o %cpu -o rss -o time -o command -r > ps_tmp
         idle_size=`grep idle\] ps_tmp | grep -v grep | awk '{ print $3 }'`
-        total_size=`awk '{ size+=$3 } END { print size}' ps_tmp`
+        total_size=`awk '{ if(NR > 1) {size+=$3} } END { print size}' ps_tmp`
+        usage_size=`echo "$total_size - $idle_size" | bc`
+        echo "$usage_size * 100 / $total_size" | bc >> cpuusage
+        echo "TOP 5 processes are:"
+        echo "==============================================================="
+        cat ps_tmp | grep -v idle\] | head -n 6
+        
+        
         sleep 1
     done
+    exit
 fi
